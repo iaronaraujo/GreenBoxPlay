dd0c095b4187c6182e09229032ccd83338d168ca
Iaron da Costa Araujo <iaron.araujo@ccc.ufcg.edu.br>
2016-07-21 13:01:43 -0300

    I think the first user story is working

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -1,8 +1,14 @@
 package controllers;
 
+import com.google.inject.Inject;
+import models.User;
+import play.data.FormFactory;
 import play.mvc.*;
 
 import views.html.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This controller contains an action to handle HTTP requests
@@ -16,8 +22,23 @@ public class HomeController extends Controller {
      * this method will be called when the application receives a
      * <code>GET</code> request with a path of <code>/</code>.
      */
+    //public Result index() {
+    //    return ok(index.render("Your new application is ready."));
+    //}
+    private List<User> userList = new ArrayList<>();
+
+    @Inject
+    private FormFactory formFactory;
+
+
     public Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(views.html.index.render(userList));
+    }
+
+    public Result registerUser(){
+        User user = formFactory.form(User.class).bindFromRequest().get();
+        userList.add(user);
+        return redirect(routes.HomeController.index());
     }
 
 }
